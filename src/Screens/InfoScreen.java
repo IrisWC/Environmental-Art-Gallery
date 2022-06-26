663c46681246ea9e65248032c6d81662d3907ac6
Iris Chou <iris.w.chou@gmail.com>
2022-06-26 12:42:41 -0700

    New Info screen

--- src/Screens/InfoScreen.java
+++ src/Screens/InfoScreen.java
@@ -19,6 +19,8 @@ public class InfoScreen extends Screen{
 	public void draw() {
 		surface.background(255);
 		
+		surface.image(surface.loadImage("img\\Info.png"), 0, 0, 1600, 900);
+		
 		surface.noFill();
 		surface.rect((float)backButton.x, (float)backButton.y, (float)backButton.width, (float)backButton.height);
 	
@@ -27,17 +29,18 @@ public class InfoScreen extends Screen{
 		surface.textAlign(surface.CENTER, surface.CENTER);
 		surface.text("<", 20, 10, 100, 100);
 		
-		surface.textSize(30);
-		surface.text("Welcome to the Environmental Art Gallery! "
-				+ "Here you can view and learn about different pieces of art from artists around the world, "
-				+ "all of which are designed to promote the environment. Each of these artistic creations "
-				+ "was either made with the intention of inspiring environmental protection or is made out of recycled materials. "
-				+ "Have fun exploring these in the gallery!", 100, 0, 600, 900);
-		surface.text("External Credits:\n\nhttps://theartling.com/en/artzine/art-highlights-climate-change/\n"
-				+ "https://olafureliasson.net/archive/artwork/WEK109190/ice-watch\n"
-				+ "https://qubahq.com/2014/07/player-walk-cycle/\n"
-				+ "https://processing.org/\n"
-				+ "https://oceanic.global/tan-zi-xi/", 900, 0, 600, 900);
+		
+//		surface.textSize(30);
+//		surface.text("Welcome to the Environmental Art Gallery! "
+//				+ "Here you can view and learn about different pieces of art from artists around the world, "
+//				+ "all of which are designed to promote the environment. Each of these artistic creations "
+//				+ "was either made with the intention of inspiring environmental protection or is made out of recycled materials. "
+//				+ "Have fun exploring these in the gallery!", 100, 0, 600, 900);
+//		surface.text("External Credits:\n\nhttps://theartling.com/en/artzine/art-highlights-climate-change/\n"
+//				+ "https://olafureliasson.net/archive/artwork/WEK109190/ice-watch\n"
+//				+ "https://qubahq.com/2014/07/player-walk-cycle/\n"
+//				+ "https://processing.org/\n"
+//				+ "https://oceanic.global/tan-zi-xi/", 900, 0, 600, 900);
 	}
 	
 	public void mousePressed() {
