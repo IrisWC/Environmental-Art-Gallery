93c6592e6fb56c5fdd953584c083c9d5d85550f6
Iris Chou <iris.w.chou@gmail.com>
2022-06-25 21:12:33 -0700

    Create InfoScreen.java

---
+++ src/Screens/InfoScreen.java
@@ -0,0 +1,17 @@
+package Screens;
+
+import Core.DrawingSurface;
+
+public class InfoScreen extends Screen{
+
+	private DrawingSurface surface;
+	
+	public InfoScreen(int width, int height, DrawingSurface drawer) {
+		super(width, height);
+		surface = drawer;
+	}
+	
+	public void draw() {
+		
+	}
+}
